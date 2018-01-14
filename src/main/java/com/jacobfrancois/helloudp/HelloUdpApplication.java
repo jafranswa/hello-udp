bd7cf9be8545dcaab1f63144dad6823f3d5a9d8c
jacob <jafranswa@gmail.com>
2018-01-14 00:48:37 -0600

    initial commit

---
+++ src/main/java/com/jacobfrancois/helloudp/HelloUdpApplication.java
@@ -0,0 +1,12 @@
+package com.jacobfrancois.helloudp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloUdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloUdpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jacobfrancois/helloudp/HelloUdpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jacobfrancois.helloudp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloUdpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
